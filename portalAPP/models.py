128c596b944b15ca644df84acb58b872efb866ce
Rajneesh <rajneeshrpg@gmail.com>
2019-11-19 22:15:27 -0300

    Criação da tabela de eventos

--- portalAPP/models.py
+++ portalAPP/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from rest_framework.permissions import IsAuthenticated
 from .managers import CustomUserManager
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Usuario (AbstractUser):
     username = None
@@ -52,3 +54,20 @@ class Noticia(models.Model):
     
     def __str__(self):
         return self.titulo
+
+class Evento(models.Model):
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='autor_evento')
+    titulo = models.CharField(max_length = 200, unique = True)
+    descricao = models.TextField(max_length = 2000)
+    imagem = models.ImageField(upload_to = 'noticiaImgs')
+    participantes = models.ManyToManyField(Usuario)
+    data_publicacao = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.titulo
+    
+    def validate_date(data):
+        if data < timezone.now():
+            raise ValidationError("Date cannot be in the past")
+    
+    data = models.DateTimeField(validators=[validate_date])
